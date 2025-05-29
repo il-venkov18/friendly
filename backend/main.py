73d1d776c9c63918a6165b18dca5bb2c8765c96f
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2025-05-29 16:37:58 +0300

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,77 +1,59 @@
-# import hashlib
-# import hmac
-#
-# from fastapi import FastAPI, HTTPException
-# from fastapi.middleware.cors import CORSMiddleware
-# from pydantic import BaseModel
-#
-# app = FastAPI()
-# BOT_TOKEN = "7531358236:AAEslLEWRJKwklbcFA-hB1qc4Uw2NVAX7AQ"
-#
-# app.add_middleware(
-#        CORSMiddleware,
-#        allow_origins=["*"],  # Укажите разрешенные источники
-#        allow_credentials=True,
-#        allow_methods=["*"],
-#        allow_headers=["*"],
-#    )
-#
-#
-# def check_telegram_auth(data: dict, bot_token: str) -> bool:
-#     print('test')
-#     auth_data = data.copy()
-#     hash_to_check = auth_data.pop("hash")
-#     print(hash_to_check)
-#
-#     data_check_string = '\n'.join(
-#         f"{k}={v}" for k, v in sorted(auth_data.items())
-#     )
-#
-#     secret_key = hashlib.sha256(bot_token.encode()).digest()
-#     calculated_hash = hmac.new(secret_key, data_check_string.encode('utf-8'), hashlib.sha256).hexdigest()
-#     print(calculated_hash)
-#
-#     print(hmac.compare_digest(calculated_hash, hash_to_check))
-#     return hmac.compare_digest(calculated_hash, hash_to_check)
-#
-#
-# class TelegramAuthData(BaseModel):
-#     id: int
-#     first_name: str
-#     username: str | None
-#     photo_url: str | None
-#     auth_date: int
-#     hash: str
-#
-#
-# @app.post("/auth/telegram")
-# async def auth_telegram(data: TelegramAuthData):
-#     if not check_telegram_auth(data.dict(), BOT_TOKEN):
-#         raise HTTPException(status_code=403, detail="Invalid Telegram data")
-#
-#     # Тут можно создать/найти пользователя в БД по `id`
-#     # и вернуть токен/куки сессии.
-#
-#     return {"status": "ok", "user_id": data.id}
-#
-#
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from init_data_py import InitData
 
-TELEGRAM_BOT_TOKEN = "7531358236:AAEslLEWRJKwklbcFA-hB1qc4Uw2NVAX7AQ"  # из BotFather
-
 app = FastAPI()
+BOT_TOKEN = "7531358236:AAEslLEWRJKwklbcFA-hB1qc4Uw2NVAX7AQ"
+
+app.add_middleware(
+       CORSMiddleware,
+       allow_origins=["*"],  # Укажите разрешенные источники
+       allow_credentials=True,
+       allow_methods=["*"],
+       allow_headers=["*"],
+   )
 
 
-@app.post("/auth/verify")
-def verify(data):
+def check_telegram_auth(data: str, bot_token: str) -> bool:
     print('Данные пользователя: ', data)
     init_data = InitData.parse(data)
 
     is_valid = init_data.validate(
-        bot_token=TELEGRAM_BOT_TOKEN,
+        bot_token=bot_token,
         lifetime=3600,
     )
     print(is_valid)
     return is_valid
+
+
+@app.post("/auth/telegram")
+async def auth_telegram(data):
+    if not check_telegram_auth(data, BOT_TOKEN):
+        raise HTTPException(status_code=403, detail="Invalid Telegram data")
+
+    # Тут можно создать/найти пользователя в БД по `id`
+    # и вернуть токен/куки сессии.
+
+    return {"status": "ok", "user_id": data.id}
+
+
+
+# from fastapi import FastAPI
+# from init_data_py import InitData
+#
+# TELEGRAM_BOT_TOKEN = "7531358236:AAEslLEWRJKwklbcFA-hB1qc4Uw2NVAX7AQ"  # из BotFather
+#
+# app = FastAPI()
+#
+#
+# @app.post("/auth/verify")
+# def verify(data):
+#     print('Данные пользователя: ', data)
+#     init_data = InitData.parse(data)
+#
+#     is_valid = init_data.validate(
+#         bot_token=TELEGRAM_BOT_TOKEN,
+#         lifetime=3600,
+#     )
+#     print(is_valid)
+#     return is_valid
