32b3b7131107be35de8b71b44d62f06334e98bd9
Valentina <kviditch@gmail.com>
2025-06-12 16:21:49 +0200

    like functionality, layer architecture
    add service and repository layers, add exxception handling, dependency injection, abstract classes

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import BigInteger, String, Integer
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
+from app.models.likes_association_table import likes_association_table
 
 from app.core.db import Base
 
@@ -14,3 +16,17 @@ class User(Base):
     username: Mapped[str] = mapped_column(String, nullable=True)
     photo_url: Mapped[str] = mapped_column(String, nullable=True)
     auth_date: Mapped[int] = mapped_column(Integer, nullable=False)
+    liked_users: Mapped[List["User"]] = relationship(
+        secondary=likes_association_table,
+        primaryjoin=id == likes_association_table.c.user_id_from,
+        secondaryjoin=id == likes_association_table.c.user_id_to,
+    )
+    # dislikes: Mapped[List["User"]] = relationship(
+    #     primaryjoin=id == likes_association_table.c.user_id_from,
+    #     secondaryjoin=id == likes_association_table.c.user_id_to,
+    # )
+    # matches: Mapped[List["User"]] = relationship(
+    #     primaryjoin=id == matches_association_table.c.user_id_from,
+    #     secondaryjoin=id == matches_association_table.c.user_id_to,
+    #     back_populates="matches"
+    # )
