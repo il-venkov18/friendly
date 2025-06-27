3c91a71ef6acc36167ea612e04928689dacdc5d9
Andrest31 <128243532+Andrest31@users.noreply.github.com>
2025-06-28 02:00:21 +0300

    fix

--- frontend/src/app/providers/style-provider.tsx
+++ frontend/src/app/providers/style-provider.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@/shared/ui/theme';
+import { ThemeProvider } from '@/shared/ui/theme/theme-provider';
 import { ReactNode } from 'react';
 
 export const StyleProvider = ({ children }: { children: ReactNode }) => (
